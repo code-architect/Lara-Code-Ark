538886ec872566208213168738a7e84cdfaa6249
code-architect <codearchitectdeveloper@gmail.com>
2018-01-29 12:01:17 +0530

    Re-factoring the template, now its showing the proper template struct

---
+++ resources/views/layouts/partials/frontend/frontend_partial_nav.blade.php
@@ -0,0 +1,27 @@
+<section class="hero">
+    <header>
+        <div class="wrapper">
+            <a href="#"><img src="{{ asset('css/frontend/img/logo.png') }}" class="logo" alt="" titl=""/></a>
+            <a href="#" class="hamburger"></a>
+            <nav>
+                <ul>
+                    <li><a href="#">Buy</a></li>
+                    <li><a href="#">Rent</a></li>
+                    <li><a href="#">Sell</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                @if (Auth::guest())
+                    <a href="{{ route('login') }}" class="login_btn">Login</a>
+                   <a href="{{ route('register') }}" class="login_btn">Register</a>
+                @endif
+
+            </nav>
+        </div>
+    </header><!--  end header section  -->
+
+    <section class="caption">
+        <h2 class="caption" style="color: black">Find You Dream Home</h2>
+        <h3 class="properties">Appartements - Houses - Mansions</h3>
+    </section>
+</section><!--  end hero section  -->
